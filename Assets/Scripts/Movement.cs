166144d1bb3f390f35b00ba081f363d0ce7b1b1c
Claire1605 <claire1605@gmail.com>
2017-08-05 16:18:42 +0100

    Movement Script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
     public  Rigidbody2D flarePlane;
     public Rigidbody2D bombPlane;
-	// Use this for initialization
+    public float vertSpeed;
+    public float horizSpeed;
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-      //  flarePlane.position.y += Input.GetAxis("Vertical");
-       // flarePlane.Translate(0, Input.GetAxis("Vertical"), 0);
-       // bombPlane.Translate(0, Input.GetAxis("Vertical"), 0);
-        flarePlane.velocity += new Vector2(0, Input.GetAxis("Vertical")/2);
-        bombPlane.velocity += new Vector2(0, Input.GetAxis("Vertical")/2);
+       
+
+  //      flarePlane.velocity = new Vector2(horizSpeed, 0);
+  //      bombPlane.velocity = new Vector2(horizSpeed, 0);
+
+        flarePlane.velocity = new Vector2(horizSpeed, Input.GetAxis("Vertical") * vertSpeed);
+        bombPlane.velocity = new Vector2(horizSpeed, Input.GetAxis("Vertical") * vertSpeed);
     }
 }
