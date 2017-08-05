5d10f4f1d97c70cd795f918948e2de63bfa63576
Claire1605 <claire1605@gmail.com>
2017-08-05 15:46:46 +0100

    First Commit]

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public  Rigidbody2D flarePlane;
+    public Rigidbody2D bombPlane;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      //  flarePlane.position.y += Input.GetAxis("Vertical");
+       // flarePlane.Translate(0, Input.GetAxis("Vertical"), 0);
+       // bombPlane.Translate(0, Input.GetAxis("Vertical"), 0);
+        flarePlane.velocity += new Vector2(0, Input.GetAxis("Vertical")/2);
+        bombPlane.velocity += new Vector2(0, Input.GetAxis("Vertical")/2);
+    }
+}
